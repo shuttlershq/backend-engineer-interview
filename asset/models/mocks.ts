c24319f32cc8d896238e593ac2f2877e49646f3c
Francis Ilechukwu <francis94c@gmail.com>
2021-12-16 15:41:18 +0100

    Mock Data: Random Locations.

---
+++ asset/models/mocks.ts
@@ -0,0 +1,24 @@
+import { Trail } from "../types/trail";
+
+const hypotheticalTrailSets: Array<Array<Trail>> = [
+    [
+        { latitude: 6.4971774863170415, longitude: 3.3261323115578154 },
+        { latitude: 6.498819118058466, longitude: 3.326647318098925 },
+        { latitude: 6.500013026809694, longitude: 3.3281064805591583 },
+        { latitude: 6.5003967824137545, longitude: 3.3287716900341686 },
+        { latitude: 6.501334859569267, longitude: 3.329758790140195 },
+        { latitude: 6.501347025207026, longitude: 3.3297716672668503 },
+        { latitude: 6.504351454296718, longitude: 3.3356197876459746 },
+        { latitude: 6.5044010585925, longitude: 3.3356647824137545 },
+        { latitude: 6.512564046185491, longitude: 3.3334603022275235 }
+    ],
+    [
+        { latitude: 6.509123663853103, longitude: 3.317171590401396 },
+        { latitude: 6.512625060145451, longitude: 3.32100299081813 },
+        { latitude: 6.514355371048466, longitude: 3.3115573272686705 },
+        { latitude: 6.50565166076479, longitude: 3.310476896218592 },
+        { latitude: 6.513786627242836, longitude: 3.310599140948113 }
+    ]
+];
+
+export default hypotheticalTrailSets;
