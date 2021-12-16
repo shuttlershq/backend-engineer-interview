21c94ffcf083e931f29e74bf664619730290baee
Francis Ilechukwu <francis94c@gmail.com>
2021-12-16 15:42:35 +0100

    Create location_update_controller.ts

---
+++ asset/controllers/location_update_controller.ts
@@ -0,0 +1,36 @@
+import hypotheticalTrailSets from "../models/mocks";
+import { getRandomNumber } from "../helpers/integer_helper";
+import { generatePostMethodHeader } from "../helpers/http_helper";
+import { Trail } from './../types/trail';
+import { postOptions } from "../config/http";
+
+declare function require(name:string): any;
+
+const http = require('http');
+
+class LocationUpdateController {
+    broadcastLocation(assetId: number) {
+        let trail: Trail = hypotheticalTrailSets[assetId][getRandomNumber(0, hypotheticalTrailSets.length - 1)];
+
+        let body = {
+            latitude: trail.latitude,
+            longitude: trail.longitude
+        };
+
+        console.log(`Broadcasting Trail ${trail.latitude}:${trail.longitude}`);
+
+        const req = http.request({ ...postOptions, path: `/v1/assets/${assetId}/location`, method: 'POST' }, (res: any) => {
+            if (res.statusCode == 204) {
+                console.log("Successfully broadcasted location");
+            }
+        })
+
+        req.on('error', (error: any) => {
+            console.error(error);
+        })
+
+        req.end();
+    }
+}
+
+export default new LocationUpdateController();
